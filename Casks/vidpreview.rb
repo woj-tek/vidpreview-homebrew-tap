70f25261a980df00c99067708a7b9d5cbaa56931
Tarun <me@tarun.uk>
2026-04-26 20:07:46 +0100

    Update VidPreview version and simplify cask

--- Casks/vidpreview.rb
+++ Casks/vidpreview.rb
@@ -1,17 +1,8 @@
 cask "vidpreview" do
-  version "5.1"
+  version "5.1B"
+  sha256 "5ec97c4c3f0d8d5032a64e91ec67ad03b82f85a089e3ae82a78d88c305b3a103"
 
-  on_arm do
-    sha256 "5ec97c4c3f0d8d5032a64e91ec67ad03b82f85a089e3ae82a78d88c305b3a103"
-
-    url "https://github.com/ptaru/VidPreview/releases/download/#{version}/VidPreview_arm64.zip"
-  end
-  on_intel do
-    sha256 "6e2a007a8f85b3abd524553f5d05b06869588cb1a6e413bbe51b787f87dbf8be"
-
-    url "https://github.com/ptaru/VidPreview/releases/download/#{version}/VidPreview_universal.zip"
-  end
-
+  url "https://github.com/ptaru/VidPreview/releases/download/#{version}/VidPreview.zip"
   name "VidPreview"
   desc "QuickLook extension for native video preview"
   homepage "https://github.com/ptaru/VidPreview"
@@ -24,9 +15,4 @@ cask "vidpreview" do
   depends_on macos: ">= :sequoia"
 
   app "VidPreview.app"
-
-  postflight do
-    system_command "/usr/bin/xattr",
-                   args: ["-cr", "#{appdir}/VidPreview.app"]
-  end
 end
