c387ec48574f66c7eca9d65a3baaded878475e8a
Wojtek <woj-tek@users.noreply.github.com>
2026-06-12 12:32:43 +0200

    Update depends_on format as per homebrew indications

--- Casks/privateclient.rb
+++ Casks/privateclient.rb
@@ -12,7 +12,7 @@ cask "privateclient" do
     strategy :github_latest
   end
 
-  depends_on macos: ">= :tahoe"
+  depends_on macos: :tahoe
 
   app "PrivateClient.app"
 end
--- Casks/vidpreview.rb
+++ Casks/vidpreview.rb
@@ -12,7 +12,7 @@ cask "vidpreview" do
     strategy :github_latest
   end
 
-  depends_on macos: ">= :sequoia"
+  depends_on macos: :sequoia
 
   app "VidPreview.app"
 end
